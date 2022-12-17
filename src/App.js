be52cdde6d7a1ca95f0826907baa83a95df1fae6
ArchiesLaywhyeejr <115646170+ArchiesLaywhyeejr@users.noreply.github.com>
2022-12-17 21:09:11 +0000

    first

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <div class='font-bold'>
+     <h1>
+      Hello World
+      </h1>
+    </div>
+    </>
+  );
+}
+
+export default App;
